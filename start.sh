a204444c8924f259f7b27b09c13732bcc84c80eb
Jason Aricheta <jason.aricheta@gmail.com>
2023-04-09 10:33:56 +1200

    working constants deletion

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cp knowledgebase-constants.json ./backend/knowledgebase-constants.json && cp knowledgebase-constants.json ./frontend/knowledgebase-constants.json
+
 cd ./backend && npm run start
