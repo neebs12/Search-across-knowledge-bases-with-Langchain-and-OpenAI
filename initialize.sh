ec249265d118b88e4bf35a22924b7d7a833be417
Jason Aricheta <jason@hnry.io>
2023-04-11 08:41:20 +1200

    initialization fixes

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,3 @@
+cp knowledgebase-constants.json ./backend/knowledgebase-constants.json && cp knowledgebase-constants.json ./frontend/knowledgebase-constants.json
 cd ./backend && npm i && npm run ingest
-cd ./frontend && npm i
+cd ../frontend && npm i
